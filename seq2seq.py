59143fdc593b3d3b56f513803190626d36158103
duyongan <13261051171@163.com>
2018-09-08 08:45:46 +0800

    Update seq2seq.py
    更改一些错误

--- seq2seq.py
+++ seq2seq.py
@@ -8,19 +8,21 @@ from keras.models import Model,load_model
 from keras.layers.core import Reshape
 
 
-lenth_of_seq=20
-vocab_size=4000
+lenth_of_seq=len(xs[0])
+vocab_size=len(tokenizer.word_index)+1
+out_length=len(ys[0])
+
 x = Input(shape=(lenth_of_seq,))
-x_embedding = Embedding(vocab_size, 50, input_length=lenth_of_seq)(x)
+x_embedding = Embedding(vocab_size, 100, input_length=lenth_of_seq)(x)
 hidden_layer = GRU(100,return_sequences=True)(x_embedding)
 mid_list=[]
-for i in range(lenth_of_seq):
+for i in range(out_length):
     mid = Dense(5)(hidden_layer)
-    mid = Reshape((1,100))(mid)
+    mid = Reshape((1,lenth_of_seq*5))(mid)
     mid_list.append(mid)
 mid_2=concatenate(mid_list,axis=1)
 y = GRU(100,return_sequences=True)(mid_2)
+y=Dense(vocab_size,activation='softmax')(y)
 model = Model(inputs=x, outputs=y)
-y=Dense(5,activation='softmax')(y)
-model.compile(loss='binary_crossentropy', optimizer='Adam',metrics=['acc'])
+model.compile(loss='categorical_crossentropy', optimizer='Adam',metrics=['acc'])
 model.summary()
